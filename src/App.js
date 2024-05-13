50839d15d33a7635fe9de3aa774a8d98befa7b46
Joinjun001 <injoon00280@gmail.com>
2024-05-13 19:06:33 +0900

    feat: 사운드 이펙트 추가 및 모서리 버그 수정완료

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const audio = new Audio("/music/MP_Cupboard Door Close.mp3");
+
+  function playAudio() {
+    audio.currentTime = 0; // 오디오를 처음부터 재생
+    audio.play();
+  }
+  // 속도함수에 랜덤으로 넣어줄 랜덤 숫자 함수
+  function getRandomNumber() {
+    return Math.random();
+  }
   const canvasRef = useRef(null);
   const canvasWidth = 600;
   const canvasHeight = 600;
@@ -24,12 +34,28 @@ function App() {
   let lightness = 50; // 명도
 
   useEffect(() => {
+    var AudioContext;
+    var audioContext;
+
+    window.onload = function () {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(() => {
+          AudioContext = window.AudioContext || window.webkitAudioContext;
+          audioContext = new AudioContext();
+        })
+        .catch((e) => {
+          console.error(`Audio permissions denied: ${e}`);
+        });
+    };
+
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
     let animationFrameId;
     let lastRenderTime = Date.now(); // 프레임간의 시간차를 계산하기 위함
 
     function increaseBall() {
+      playAudio();
       ballWidth += 2;
       ballHeight += 2;
       setBall1((prev) => prev + 2);
@@ -43,44 +69,118 @@ function App() {
       lastRenderTime = currentTime;
 
       if (ballWidth >= canvasWidth) {
-        cancelAnimationFrame(animationFrameId); // ballWidth가 100 이상이면 애니메이션 종료
+        cancelAnimationFrame(animationFrameId); // 공이 화면에 가득차면 애니메이션 종료
         return;
       }
 
+      // 공이 위쪽 벽에 부딪혔을때
       if (ballPosition.y < 0) {
         count += 1;
         increaseBall();
         ballPosition.y = 0; // 공을 0 위치에 놓고
 
-        ballVelocity.y -= 0.2;
+        // 모서리에 안찡기게 하는 if 문
+        if (ballPosition.x < 0) {
+          ballPosition.x = 0;
+        } else if (ballPosition.x > canvasWidth - ballWidth) {
+          ballPosition.x = canvasWidth - ballWidth;
+        }
+
+        // 부딪히면서 속도 변경
+        const result = getRandomNumber();
+        const result2 = getRandomNumber();
+        ballVelocity.y -= result;
         ballVelocity.y = -ballVelocity.y; // 속도의 방향을 반대로 바꿉니다.
-        setBall4((prev) => (prev -= 0.2));
+        if (ballVelocity.x < 0) {
+          ballVelocity.x -= result2;
+          setBall3((prev) => (prev -= result2));
+        } else {
+          ballVelocity.x += result2;
+          setBall3((prev) => (prev += result2));
+        }
+        setBall4((prev) => (prev -= result));
         setBall4((prev) => -prev);
       } else if (ballPosition.y > canvasHeight - ballHeight) {
         count += 1;
         increaseBall();
+
         ballPosition.y = canvasHeight - ballHeight; // 공을 캔버스의 최대 높이에 놓고
 
-        ballVelocity.y += 0.2;
+        // 모서리에 안찡기게 하는 if 문
+        if (ballPosition.x < 0) {
+          ballPosition.x = 0;
+        } else if (ballPosition.x > canvasWidth - ballWidth) {
+          ballPosition.x = canvasWidth - ballWidth;
+        }
+
+        // 부딪히면서 속도 변경
+        const result = getRandomNumber();
+        const result2 = getRandomNumber();
+        ballVelocity.y += result;
         ballVelocity.y = -ballVelocity.y; // 속도의 방향을 반대로 바꿉니다.
-        setBall4((prev) => (prev += 0.2));
+
+        if (ballVelocity.x < 0) {
+          ballVelocity.x -= result2;
+          setBall3((prev) => (prev -= result2));
+        } else {
+          ballVelocity.x += result2;
+          setBall3((prev) => (prev += result2));
+        }
+        setBall4((prev) => (prev += result));
         setBall4((prev) => -prev);
       } else if (ballPosition.x < 0) {
+        // 공이 왼쪽으로 부딪혔을때
+
+        // 모서리에 안찡기게 하는 if 문
+        if (ballPosition.y < 0) {
+          ballPosition.y = 0;
+        } else if (ballPosition.y > canvasHeight - ballHeight) {
+          ballPosition.y = canvasHeight - ballHeight;
+        }
         count += 1;
         increaseBall();
 
+        const result = getRandomNumber();
+        const result2 = getRandomNumber();
         ballPosition.x = 0; // 공을 0 위치에 놓고
-        ballVelocity.x -= 0.2;
+        ballVelocity.x -= result;
         ballVelocity.x = -ballVelocity.x; // 속도의 방향을 반대로 바꿉니다.
-        setBall3((prev) => (prev -= 0.2));
+        if (ballVelocity.y < 0) {
+          ballVelocity.y -= result2;
+          setBall4((prev) => (prev -= result2));
+        } else {
+          ballVelocity.y += result2;
+          setBall4((prev) => (prev += result2));
+        }
+
+        setBall3((prev) => (prev -= result));
         setBall3((prev) => -prev);
       } else if (ballPosition.x > canvasWidth - ballWidth) {
         count += 1;
         increaseBall();
+
+        // 모서리에 안찡기게 하는 if 문
+        if (ballPosition.y < 0) {
+          ballPosition.y = 0;
+        } else if (ballPosition.y > canvasHeight - ballHeight) {
+          ballPosition.y = canvasHeight - ballHeight;
+        }
+
+        const result = getRandomNumber();
+        const result2 = getRandomNumber();
         ballPosition.x = canvasWidth - ballWidth; // 공을 캔버스의 최대 높이에 놓고
-        ballVelocity.x += 0.2;
+        ballVelocity.x += result;
         ballVelocity.x = -ballVelocity.x; // 속도의 방향을 반대로 바꿉니다.
-        setBall3((prev) => (prev += 0.2));
+
+        if (ballVelocity.y < 0) {
+          ballVelocity.y -= result2;
+          setBall4((prev) => (prev -= result2));
+        } else {
+          ballVelocity.y += result2;
+          setBall4((prev) => (prev += result2));
+        }
+
+        setBall3((prev) => (prev += result));
         setBall3((prev) => -prev);
       } else {
         ballVelocity.y += ballAcceleration.y;
@@ -117,7 +217,10 @@ function App() {
   return (
     <div className="App">
       <div className="title">
-        화면에 가득찰 때 까지 공이 커지고 속도가 빨라집니다{" "}
+        화면에 가득찰 때 까지 공이 커지고 속도가 빨라집니다
+      </div>
+      <div className="title">
+        속도는 벽에 부딪힐때마다 0~1사이의 랜덤한 숫자만큼 증가합니다.
       </div>
       <div className="title">
         크기 : {ball1} / x축 속도 : {ball3.toFixed(1)} / y축 속도 :{" "}
